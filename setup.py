a569bbde9cd0b8999337731c6b6603ec03d83d01
Dusan Klinec <dusan.klinec@gmail.com>
2023-01-25 14:21:16 +0100

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.0.2'
+version = '0.0.3'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
@@ -17,7 +17,7 @@ install_requires = [
     'requests',
     'jsonpath_ng',
     'PyJWT==2.6.0',
-    'ph4-monitlib',
+    'ph4-monitlib>=0.0.3',
 ]
 
 dev_extras = [
